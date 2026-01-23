eb6e6be8190d654cd122b2ebcc8d376abfb1f677
Sudarshan-N-2003 <sudarshann.cs@vvit.ac.in>
2026-01-23 14:24:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -189,8 +189,8 @@ a:hover {
         </div>
 
         <div class="content">
-            <h1>Welcome to the AMC College Exam Section</h1>
-            <p>Prepare your question papers and manage exam schedules directly online!</p>
+            <h1>Welcome to the VVIT College Exam Section</h1>
+            <p>Education is a Powerful Weapon!</p>
         </div>
         
         <div class="buttons">
